b7790cb218ef2413ab105b8624fcb85fc1567348
Vince Lee <vlee@BES.blackstoneenergy.com>
2018-06-04 14:58:08 -0400

    Calling a component from another component

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'events-app',
     //Note the character below is the CONSOLE KEY, not a quote
-    template: `
-    <h2> Harros World</h2>
-    <img src="/assets/images/basic-shield.png" />
-
-  `
+    template: '<events-list></events-list>'
 })
 export class EventsAppComponent {
     title = 'app';
