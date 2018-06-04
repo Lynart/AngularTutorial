9d6f9ff26a36d5bd205a178d3e55546037396cb9
Vince Lee <vlee@BES.blackstoneenergy.com>
2018-06-04 14:08:08 -0400

    Example of how to display an asset/image Notes have references for how this works; refer to cli bullet

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-app',
-  template: '<h2>Harros World</h2>'
+    selector: 'events-app',
+    //Note the character below is the CONSOLE KEY, not a quote
+    template: `
+    <h2> Harros World</h2>
+    <img src="/assets/images/basic-shield.png" />
+
+  `
 })
 export class EventsAppComponent {
-  title = 'app';
+    title = 'app';
 }
